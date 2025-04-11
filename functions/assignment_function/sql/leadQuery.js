4f00c4d33dae02de84c31d80f4449c168467e648
Jagdeep-Trigya <jagdeep.sopran@trigya.co>
2025-04-11 16:33:34 +0530

    lead to deal

--- functions/assignment_function/sql/leadQuery.js
+++ functions/assignment_function/sql/leadQuery.js
@@ -1,6 +1,6 @@
 module.exports = {
     getLeadById: "Select * from leads where ROWID = ",
-    getAllLead:"Select * from leads",
+    getAllLead:`Select * from leads where status != 'Converted to Deal'`,
     deleteLead: "Delete from leads where ROWID = ",
     leadCount:"Select count(ROWID) as total from leads",
 }
